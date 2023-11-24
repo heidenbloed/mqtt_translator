d4e0c2ca028d4f4650b1104251aaa54584fad93c
heidenbloed <git@heidenblut.eu>
2023-11-24 21:25:56 +0100

    Listen for octoprint progress messages and report to tube lamp.

--- mqtt_translator.py
+++ mqtt_translator.py
@@ -1,14 +1,17 @@
 import paho.mqtt.client as mqtt
+import json
 import mqtt_secrets
 
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}.")
-    client.subscribe("#")
+    client.subscribe("octoprint/progress/printing")
 
 
 def on_message(client, userdata, msg):
-    print(f"[{msg.topic}] {msg.payload}")
+    printing_progress_info = json.loads(msg.payload)
+    progress = printing_progress_info["progress"]
+    client.publish("lamps/tube/progress", str(progress))
 
 
 def main():
